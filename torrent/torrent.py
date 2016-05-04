9e1f2b583a539c458bc706edd5f36eca90a3e353
Canule <frikandal@gmail.com>
2016-05-04 19:36:42 +0200

    Torrent Search
    @kat.cr

---
+++ torrent/torrent.py
@@ -0,0 +1,94 @@
+from .utils.dataIO import fileIO
+from .utils import checks
+from __main__ import send_cmd_help
+from __main__ import settings as bot_settings
+# Sys.
+from operator import itemgetter, attrgetter
+import discord
+from discord.ext import commands
+#from copy import deepcopy
+#import aiohttp
+#import asyncio
+import json
+import os
+
+try:
+    import ktorrent
+    ktorrent_available = True
+except:
+    ktorrent_available = False
+
+
+class Torrent:
+    """Search for torrents"""
+    def __init__(self,bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True, no_pm=False)
+    async def torrent(self, ctx, *, text):
+        
+        if text == ():
+            return
+        text = "".join(text)
+        print(text)        
+        # Basic Search
+        search = ktorrent.search(search=text)
+        # Complex Search
+        #search = ktorrent.search(search='Linux Shell script', strict=1, category='books', field='age', sorder='desc', page=2)
+        #print (search)
+        # Top books
+        #search = ktorrent.top(category=text)
+        # Top movies
+        #search = ktorrent.top(category=text, page=2)
+        #print (top_books)
+        """
+        "meta"
+            "pageCurrent"
+            "pageResult"
+            "pageTotal"
+            //
+			"age"
+			"category"
+			"files"
+			"leech"
+			"link"
+			"magnet"
+			"name"
+			"seed"
+			"size"
+			"verified"
+			"web"
+        """
+        
+        print (search)
+        d = json.loads(search)
+        
+        if len(d) > 1:
+            list = "\n"
+            index = 0
+            for items in range(0, len(d)):
+                index += 1
+                item = d["torrent"][items]
+                name = item["name"][:200] 
+                list = list + "**[{6}]\n       Name: **{0}\n       **Age: **{1}\n**       Leech/Seed: **{2}/{3}\n**       Size: **{4}\n**       Link: **{5}\n\n".format(name, item["age"], item["seed"], item["leech"], item["size"], item["link"], str(index))
+            await self.bot.say(list)
+        else:      
+            await self.bot.say("shrug")
+        
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Set-up
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
+class ModuleNotFound(Exception):
+    def __init__(self, m):
+        self.message = m
+    def __str__(self):
+        return self.message        
+
+def setup(bot):
+    if not ktorrent_available:
+        raise ModuleNotFound("ktorrent is not installed. Do 'pip3 install ktorrent --upgrade' to use this cog.")
+        return
+    bot.add_cog(Torrent(bot))
+
+
