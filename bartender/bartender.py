72396e80e89c6017a84bc74cd0d6bd7a3a19d339
Canule <frikandal@gmail.com>
2016-05-06 12:53:53 +0200

    econ intergration

--- bartender/bartender.py
+++ bartender/bartender.py
@@ -11,20 +11,44 @@ from discord.ext import commands
 #import asyncio
 import json
 import os
+import time
 
+
+try:
+    from PIL import Image, ImageDraw, ImageFont
+    pil_available = True
+except:
+    pil_available = False
+
+
+DIR_DATA = "data/bartender"
+SETTINGS = DIR_DATA+"/settings.json"
+BACKUP = DIR_DATA+"/bank_backup.json"
+BANK = "data/economy/bank.json"
 
 class Bartender:
     """Buy a drink at the bar"""
     def __init__(self,bot):
         self.bot = bot
-        self.items = [["beer", ":beer:", 2], ["wine", ":wine_glass:", 2], ["cocktail", ":cocktail:", 4], ["tropical", ":tropical_drink:", 5], ["sake", ":sake:", 4], ["champagne", ":champagne:", 30], ["tea", ":tea:", 1], ["coffee", ":coffee::", 1]]
+        self.settings = fileIO(SETTINGS, "load")
+        self.items = [["beer", ":beer:", 2], ["wine", ":wine_glass:", 2], ["cocktail", ":cocktail:", 4], ["tropical", ":tropical_drink:", 5], ["sake", ":sake:", 4], ["champagne", ":champagne:", 30], ["tea", ":tea:", 1], ["coffee", ":coffee:", 1]]
         self.numbers = ["one", "two", "tree", "four", "five", "six", "seven", "eight", "nine", "ten"]
-        self.text= ["one", "two", "tree", "four", "five", "six", "seven", "eight", "nine", "ten"]
-        self.bank = fileIO("data/economy/bank.json", "load")
-
+        
     @commands.command(pass_context=True, no_pm=False)
     async def buy(self, ctx, amount : int, drink):
         """Buy a drink"""
+        botuser = self.bot.user
+        content = ctx.message.content      
+        mentions = ctx.message.mentions       
+        author = ctx.message.author
+        
+        #Get Economy data
+        if self.econ_interlink() != None:
+            econ = self.econ_interlink()
+        else:
+            await self.bot.say("[#1] We are closed atm.")
+            return
+        
         price = -1
         icon = ""
         available = False
@@ -39,70 +63,116 @@ class Bartender:
         if not available:
             await self.bot.say("Im sorry to dissapoint you, we dont serve {}".format(drink))
             return
-                
-        content = ctx.message.content      
-        mentions = ctx.message.mentions       
-        author = ctx.message.author
+
         buy_for = []
         for member in mentions:
             buy_for.append(member.mention)
         buy_for = " ".join(buy_for)
+
         
-        if self.enough_money(author.id, price):
-            if self.withdraw_money(author.id, price):
-                drinks = ""
-                for d in range(0,amount):
-                    drinks = drinks+icon
-                if buy_for == "":
-                    await self.bot.say("{} There you go mate {}".format(buy_for, drinks))
-                else:
-                    await self.bot.say("{0} Have some {1} from {2}{3}".format(buy_for, drink, author, drinks))
+        if econ.enough_money(author.id, price):
+            econ.withdraw_money(author.id, price)
+            econ.add_money(botuser.id, price)
+             
+            drinks = ""
+            for d in range(0,amount):
+                drinks = drinks+icon
+            if buy_for == "":
+                await self.bot.say("{} There you go mate {}".format(buy_for, drinks))
             else:
-                await self.bot.say("I don't accept marbles")          
+                if amount > 1:
+                    await self.bot.say("{0} Have some {1}'s from {2}{3}".format(buy_for, drink, author, drinks))                    
+                else:    
+                    await self.bot.say("{0} Have some {1} from {2}{3}".format(buy_for, drink, author, drinks)) 
         else:
             try:
                 text_num = self.numbers[amount-1]
             except Exception as e:
                 text_num = str(amount)
             await self.bot.say("{0} Sorry mate, you don't have enough money for {1} {2}.\n it cost's {3}".format(author.mention, text_num, drink, price))
-           
-    def account_check(self, id):
-        if id in self.bank:
-            return True
-        else:
-            return False
-            
-    def enough_money(self, id, amount):
-        if self.account_check(id):
-            if self.bank[id]["balance"] >= int(amount):
-                return True
-            else:          
-                return False
-        else:      
-            return False
-            
-    def withdraw_money(self, id, amount):
-        if self.account_check(id):
-            if self.bank[id]["balance"] >= int(amount):
-                self.bank[id]["balance"] = self.bank[id]["balance"] - int(amount)
-                fileIO("data/economy/bank.json", "save", self.bank)             
-                return True
-            else:
-                return False
-        else:
-            return False
 
+    @commands.command(pass_context=True, no_pm=False)
+    async def beverages(self, ctx):
+        msg = "s"
+        for b in self.items:
+            msg = msg + b[0]
+        await self.bot.say(msg)
+        
+    #if Economy.py updates, this may break
+    @commands.command(pass_context=True)
+    @checks.is_owner()
+    async def registerbar(self, ctx, agree : str):
+        """Opens the bar and registers the bot into Economy.py bank. 
+        WARNING: Edits Economy.py's bank.json file.
+        If Economy.py is updated in the future with a different format for bank.json, the data in bank.json (everyone's balances) may be lost.
+        This command will try to save a backup in data/economytrickle/bank-backup.json in case that happens.
+        If you understand this and still want to register your bot in the bank, type:
+        [p]openbar "I have read and understand. I need my drinks!"
+        """
+        if agree.lower() == "yes":
+            econ = self.bot.get_cog('Economy')
+            bank = econ.bank
+            botuser = self.bot.user
+            if botuser.id not in bank:
+                fileIO(BACKUP, "save", bank)
+                bank[botuser.id] = {"name" : botuser.name, "balance" : 100}
+                fileIO("data/economy/bank.json", "save", bank)
+                await self.bot.say("Account opened for {}. Current balance: {}".format(botuser.mention, str(bank[botuser.id]["balance"])))             
+            else:
+                await self.bot.say("{} already has an account at the Twentysix bank.".format(botuser.mention))
+            fileIO(SETTINGS, "save", self.settings)
+        else:
+            await send_cmd_help(ctx)
+            
+    @commands.command(pass_context=True)
+    @checks.is_owner()
+    async def openbar(self, ctx):
+        """Opens the bar"""
+        author = ctx.message.author
+        self.settings["bar_startus"] = True
+        fileIO(SETTINGS, "save", self.settings)
+        await self.bot.say("{} The bar is now open!.".format(author.mention)) 
+        
+    @commands.command(pass_context=True)
+    @checks.is_owner()
+    async def closebar(self, ctx):
+        """Closes the bar"""
+        author = ctx.message.author
+        self.settings["bar_startus"] = False
+        fileIO(SETTINGS, "save", self.settings)
+        await self.bot.say("{} The bar is now closed!.".format(author.mention)) 
+
+    def econ_interlink(self):
+        econ = None
+        econ = self.bot.get_cog('Economy')
+        if econ == None:
+            print("--- Error: Was not able to load Economy cog into Economytrickle. ---")
+            return False
+        else:
+            return econ
+        
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Set-up
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def check_folders():
+    if not os.path.exists(DIR_DATA):
+        print("Creating data/bartender folder...")
+        os.makedirs(DIR_DATA)
+
+def check_files():
+    settings = {"bar_status" : False}
     
-class ModuleNotFound(Exception):
-    def __init__(self, m):
-        self.message = m
-    def __str__(self):
-        return self.message        
+    if not fileIO(SETTINGS, "check"):
+        print("Creating settings.json")
+        fileIO(SETTINGS, "save", settings)
 
 def setup(bot):
+    if not pil_available:
+        raise RuntimeError("You don't have Pillow installed, run\n```pip3 install pillow```And try again")
+        return
+    check_folders()
+    check_files()
     bot.add_cog(Bartender(bot))
 
 
