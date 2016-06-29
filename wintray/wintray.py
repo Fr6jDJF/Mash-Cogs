02f2253f85c4a6e017bed10b4d23e78e907ab54d
Canule <Canule@users.noreply.github.com>
2016-06-29 15:43:51 +0200

    Update wintray.py

--- wintray/wintray.py
+++ wintray/wintray.py
@@ -8,48 +8,6 @@ def kill_red(bot):
         try:
             if sys.platform == "linux" or sys.platform == "linux2":
                 # linux
-                """
-                    start on runlevel [2345]
-                    stop on runlevel [016]
-
-                    respawn
-                    chdir /home/username/Red-DiscordBot
-                    setuid username
-                    setgid username
-                    exec python3.5 red.py --no-prompt
-                                
-                    post-start script
-                    PID=`status app_name | egrep -oi '([0-9]+)$' | head -n1`
-                        echo $PID > /home/username/Red-DiscordBot/red.pid
-                    end script
-
-                    post-stop script
-                        rm -f /home/username/Red-DiscordBot/red.pid
-                    end script  
-                    ---------------------------------------------
-                    #http://stackoverflow.com/questions/9972023/ubuntu-upstart-and-creating-a-pid-for-monitoring
-                    #http://stackoverflow.com/questions/17856928/how-to-terminate-process-from-python-using-pid
-                    #!upstart
-                    description "Redis Server"
-
-                    env USER=redis
-
-                    start on startup
-                    stop on shutdown
-
-                    respawn
-
-                    exec sudo -u $USER sh -c "/usr/local/bin/redis-server /etc/redis/redis.conf 2>&1 >> /var/log/redis/redis.log"
-
-                    post-start script
-                        PID=`status app_name | egrep -oi '([0-9]+)$' | head -n1`
-                        echo $PID > /var/run/app_name.pid
-                    end script
-
-                    post-stop script
-                    rm -f /var/run/app_name.pid
-                    end script                         
-                """
                 bot.logout()
                 os.popen('kill -SIGTERM '+str(bat_pid))
             elif sys.platform == "darwin":
