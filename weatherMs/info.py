b0575c7ad020c5278bf155c696de90da98606907
Canule <Canule@users.noreply.github.com>
2016-07-22 12:32:27 +0200

    Update info.py

--- weatherMs/info.py
+++ weatherMs/info.py
@@ -1,6 +1,6 @@
 {
     "AUTHOR" : "Mash",
-    "INSTALL_MSG" : "Done.",
+    "INSTALL_MSG" : "Done.\n*Make sure you have the standard weather cog unloaded",
     "NAME" : "weatherMs",
     "SHORT" : "A modified version of the community's weather cog. by rfilkins1992, Dex & Will",
     "DESCRIPTION" : "Returns the temp/humidity information in a given location.\nRequires API key obtainable from www.wunderground.com/weather/api/"
