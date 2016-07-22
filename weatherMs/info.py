1aa34a5dfacebb4019dc7cae1969b50c44c89fa7
Canule <Canule@users.noreply.github.com>
2016-07-22 12:24:23 +0200

    Create info.py

---
+++ weatherMs/info.py
@@ -0,0 +1,7 @@
+{
+    "AUTHOR" : "Mash",
+    "INSTALL_MSG" : "Done.",
+    "NAME" : "weatherMs",
+    "SHORT" : "A modified version of the community's weather cog. by rfilkins1992, Dex & Will",
+    "DESCRIPTION" : "Returns the temp/humidity information in a given location.\nRequires API key obtainable from www.wunderground.com/weather/api/"
+}
