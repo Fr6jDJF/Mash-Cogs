faccdd120c408b52ee502f4ecf70827eb79784d8
Canule <Canule@users.noreply.github.com>
2016-07-28 09:29:34 +0200

    Update wintray.py

--- wintray/wintray.py
+++ wintray/wintray.py
@@ -32,3 +32,106 @@ def kill_red(bot):
         print (e)
         #bot.logout()
         return False
+
+# Available Tasks
+class StartThread():
+    def __init__ (self, instructor="unknown", thread=None):
+        self.instructor = instructor
+        self.taskname = "StartThread"
+        self.thread = thread
+
+class StopThread():
+    def __init__ (self, instructor="unknown", thread=None):
+        self.instructor = instructor
+        self.taskname = "StopThread"
+        self.thread = thread        
+        
+class StartTray():
+    def __init__ (self, instructor="unknown",):
+        self.instructor = instructor    
+        self.taskname = "StartTray"
+        
+class StopTray():
+    def __init__ (self, instructor="unknown",):
+        self.instructor = instructor    
+        self.taskname = "StopTray"
+        
+class ReloadTray():
+    def __init__ (self, instructor="unknown",):
+        self.instructor = instructor
+        self.taskname = "ReloadTray"
+        
+class QuitRed():
+    def __init__ (self, instructor="unknown",):    
+        self.instructor = instructor
+        self.taskname = "QuitRed"        
+
+class UpdateMenu():
+    def __init__ (self, instructor="unknown", data=None):
+        self.instructor = instructor
+        self.taskname = "UpdateMenu"
+        self.data = data
+                
+class HideConsole():
+    def __init__ (self, instructor="unknown"):
+        self.instructor = instructor
+        self.taskname = "HideConsole"
+        
+class ShowConsole():
+    def __init__ (self, instructor="unknown"):
+        self.instructor = instructor
+        self.taskname = "ShowConsole"
+        
+class ToggleConsole():
+    def __init__ (self, instructor="unknown"):
+        self.instructor = instructor
+        self.taskname = "ToggleConsole"        
+
+class BalloonInfo():
+    def __init__ (self, instructor="unknown", title=None, msg=None):
+        self.instructor = instructor
+        self.taskname = "BalloonInfo"
+        self.title = title
+        self.msg = msg
+
+class ToolTip():
+    def __init__ (self, instructor="unknown", tooltip=None):
+        self.instructor = instructor
+        self.taskname = "ToolTip"
+        self.tooltip = tooltip
+        
+class ChangeIcon():
+    def __init__ (self, instructor="unknown", icon=ICON_FILE):
+        self.instructor = instructor
+        self.taskname = "ChangeIcon"
+        self.icon = icon
+        
+class InstallCog():
+    def __init__ (self, instructor="unknown", cog=None):
+        self.instructor = instructor
+        self.taskname = "InstallCog"
+        self.cog = cog
+        
+class LoadCog():
+    def __init__ (self, instructor="unknown", cog=None):
+        self.instructor = instructor
+        self.taskname = "LoadCog"
+        self.cog = cog
+        
+class UnloadCog():
+    def __init__ (self, instructor="unknown", cog=None):
+        self.instructor = instructor
+        self.taskname = "UnloadCog"
+        self.cog = cog
+        
+class ReloadCog():
+    def __init__ (self, instructor="unknown", cog=None):
+        self.instructor = instructor
+        self.taskname = "ReloadCog"
+        self.cog = cog
+        
+class OpenBrowser():
+    def __init__ (self, instructor="unknown", url=None):
+        self.instructor = instructor
+        self.taskname = "OpenBrowser"
+        self.url = url
