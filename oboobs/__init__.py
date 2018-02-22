289b16b614646d2efd6483d19f6ba78f84656ca8
Canule <Canule@users.noreply.github.com>
2018-02-23 00:23:55 +0100

    Update __init__.py

--- oboobs/__init__.py
+++ oboobs/__init__.py
@@ -1,14 +1,6 @@
-from .oboobs import oboobs
-import asyncio
-import aiohttp
-import time
-import random
-import os
-import sys
-
+from .oboobs import OboobsC
 
 def setup(bot):
-    if sys.version_info < (3, 5, 0):
-        raise RuntimeError("Requires python >= 3.5")
-    else:
-bot.add_cog(oboobs(bot))
+    n = OboobsC(bot)
+    bot.add_cog(n)
+    bot.loop.create_task(n.boob_knowlegde())
